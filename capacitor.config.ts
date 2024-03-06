fcdc1fed4c42cd812bbe7d323645a4e0ae1cf91d
Ouédraogo Abdoul W <jumia@planetkoncept.pro>
2024-03-06 09:57:53 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MonPortfolio',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
